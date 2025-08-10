347c9cbdb437d83b1cfbcaa90a62c15d532ae62c
Christian Benedict A. Ramirez <benedictramirez78@gmail.com>
2025-08-11 00:13:27 +0800

    update homepage

--- src/js/script.js
+++ src/js/script.js
@@ -51,11 +51,3 @@ const form = document.getElementById('contact-form');
       });
     });
   });
-
-// AOS SCRIPT INITIALIZATION
-AOS.init();
-    const menuBtn = document.getElementById('menu-btn');
-    const mobileMenu = document.getElementById('mobile-menu');
-    menuBtn.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-    });
