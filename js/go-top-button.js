4847c83f5cdee9289644923f0b8f7dc6112d9dee
Christian Benedict A. Ramirez <benedictramirez78@gmail.com>
2025-08-18 02:54:18 +0800

    update icons to font-awesome

--- js/go-top-button.js
+++ js/go-top-button.js
@@ -6,7 +6,7 @@
   // Heroicon: Arrow Up (SVG)
   button.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
-      viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      viewBox="0 0 24 24" stroke="currentColor" stroke-width="3">
       <path stroke-linecap="round" stroke-linejoin="round" d="M5 10l7-7m0 0l7 7m-7-7v18" />
     </svg>
   `;
